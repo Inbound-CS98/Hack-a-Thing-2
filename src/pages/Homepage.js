0f9f1124b4f2af44b4b695e14a33ca97ecee4dd2
theairdemon <33183391+theairdemon@users.noreply.github.com>
2021-01-22 16:12:46 -0500

    Added API get and post calls

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import AComponent from 'components/AComponent';
-import BComponent from 'components/BComponent';
+import TalkAPI from 'components/TalkAPI';
+
+// Tutorial on React API calls
+// https://pusher.com/tutorials/consume-restful-api-react
+
+// Fetch cheatsheet
+// https://devhints.io/js-fetch
+
+const getUrl = 'http://localhost:9090/api/ping';
+
+fetch(getUrl).then(response => response.json()).then(data => console.log(data));
+
+const postUrl = 'http://localhost:9090/post';
+
+fetch(postUrl, {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json;charset=UTF-8',
+  },
+  body: JSON.stringify({
+    msg: 'hello there',
+  }),
+});
 
 const Homepage = (props) => {
   return (
     <>
       <AComponent />
-      <BComponent />
+      <TalkAPI />
     </>
   );
 };
