3714a04cabbd801eecc2c62d830dcf9b6058aaf1
theairdemon <33183391+theairdemon@users.noreply.github.com>
2021-01-22 01:02:00 -0500

    Added buttons with dynamic properties

---
+++ src/components/BComponent.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+import * as style from './AComponent.scss';
+
+const TestButton = (props) => {
+  const [isClicked, setIsClicked] = useState(true);
+
+  return (
+    <div>
+      <p className={style.title}>
+        Hi this button is named {props.name} {isClicked ? 'and you better click me right now' : ', thanks for clicking me'}
+      </p>
+      <button type="button" onClick={() => { setIsClicked(false); }}>
+        {isClicked ? 'hey bro click me right here' : 'hey thanks man i appreciate it'}
+      </button>
+    </div>
+  );
+};
+
+const BComponent = () => {
+  return (
+    <>
+      <TestButton name="FIRST-BUTTON" />
+      <TestButton name="SECOND-BUTTON" />
+      <TestButton name="Phillip" />
+    </>
+  );
+};
+
+export default BComponent;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AComponent from 'components/AComponent';
+import BComponent from 'components/BComponent';
 
 const Homepage = (props) => {
   return (
     <>
       <AComponent />
+      <BComponent />
     </>
   );
 };
