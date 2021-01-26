fde5ebda38e80c2e7c162af6c81585e7a7699c50
theairdemon <33183391+theairdemon@users.noreply.github.com>
2021-01-26 15:16:46 -0500

    Added button API post

--- src/components/TalkAPI.js
+++ src/components/TalkAPI.js
@@ -4,6 +4,20 @@ import * as style from './AComponent.scss';
 
 // test comment
 
+const postUrl = 'http://localhost:9090/post';
+/*
+fetch(postUrl, {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json;charset=UTF-8',
+  },
+  body: JSON.stringify({
+    msg: 'hello there',
+  }),
+});
+*/
+
 const TestButton = (props) => {
   const [isClicked, setIsClicked] = useState(true);
 
@@ -12,7 +26,21 @@ const TestButton = (props) => {
       <p className={style.title}>
         Hi this button is named {props.name} {isClicked ? 'and you better click me right now' : ', thanks for clicking me'}
       </p>
-      <button type="button" onClick={() => { setIsClicked(false); }}>
+      <button type="button"
+        onClick={() => {
+          setIsClicked(false);
+          fetch(postUrl, {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json;charset=UTF-8',
+            },
+            body: JSON.stringify({
+              msg: 'hello there',
+            }),
+          }).then(response => response.json()).then(data => console.log(data));
+        }}
+      >
         {isClicked ? 'hey bro click me right here' : 'hey thanks man i appreciate it'}
       </button>
     </div>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -11,18 +11,6 @@ import TalkAPI from 'components/TalkAPI';
 const getUrl = 'http://localhost:9090/api/ping';
 fetch(getUrl).then(response => response.json()).then(data => console.log(data));
 
-const postUrl = 'http://localhost:9090/post';
-fetch(postUrl, {
-  method: 'POST',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json;charset=UTF-8',
-  },
-  body: JSON.stringify({
-    msg: 'hello there',
-  }),
-});
-
 const Homepage = (props) => {
   return (
     <>
